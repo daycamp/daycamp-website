d005e700c76f00b53825c7993a20d16636f6640a
Gustavo Mendonca <gustavogmend@gmail.com>
2016-06-23 20:19:12 -0300

    adding tests

--- webapp/tests.py
+++ webapp/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 
 # Create your tests here.
-def test_daycamp(self):
-    response = self.client.get('/')
-    self.assertContains(response, 'DayCamp!', status_code=200)
+class HomePageViewTestCase(TestCase):
+
+    def test_daycamp(self):
+        response = self.client.get('/')
+        self.assertContains(response, 'DayCamp!', status_code=200)
