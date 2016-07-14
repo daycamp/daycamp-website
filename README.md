c23113c30214f10a509f95ae37e8bc085583f686
Gustavo Mendonca <gustavo@Gustavos-MacBook-Pro.local>
2016-07-13 22:03:06 -0300

    Changing a bit

--- README.md
+++
@@ -1,6 +0,0 @@
-# daycamp_website
-
-[![Build Status](https://travis-ci.org/daycamp/daycamp_website.svg?branch=master)](https://travis-ci.org/daycamp/daycamp_website)
-
-
-Simple WebPage for DayCamp.
--- webapp/tests.py
+++
@@ -1,8 +0,0 @@
-from django.test import TestCase
-
-# Create your tests here.
-class HomePageViewTestCase(TestCase):
-
-    def test_daycamp(self):
-        response = self.client.get('/')
-        self.assertContains(response, 'DayCamp!', status_code=200)
